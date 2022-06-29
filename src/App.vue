3624e1f0c49875724e3deeb582aacf2e937cb64c
coolguyhong <podolsmith@naver.com>
2022-06-29 17:08:42 +0900

    vue-cli init and config vue-router

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <tool-bar></tool-bar>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+import ToolBar from '@/components/ToolBar'
+
+export default {
+  components: {
+    ToolBar
+  },
+  methods: {
+    fetchData() {
+      console.log('hello')
+    }
+  }
+}
+</script>
+
+<style>
+body {
+  padding: 0;
+  margin: 0;
+}
+</style>
---
+++ src/routes/index.js
@@ -0,0 +1,40 @@
+import { createRouter, createWebHashHistory } from 'vue-router'
+import NewsView from '@/views/NewsView'
+import AskView from '@/views/AskView'
+import JobsView from '@/views/JobsView'
+import ItemView from '@/views/ItemView'
+import UserView from "@/views/UserView";
+
+const routes = [
+  {
+    path: '/',
+    redirect: '/news'
+  },
+  {
+    // path: url 주소
+    path: '/news',
+    // component: url 주소로 갔을 때 표시될 컴포넌트
+    component: NewsView
+  },
+  {
+    path: '/ask',
+    component: AskView
+  },
+  {
+    path: '/jobs',
+    component: JobsView
+  },
+  {
+    path: '/item',
+    component: ItemView
+  },
+  {
+    path: '/user',
+    component: UserView
+  }
+]
+
+export const router = createRouter({
+  history: createWebHashHistory(),
+  routes
+})
